3b7b4b7bf7377f2417755b13668ff3817402305e
Brian Beltran <brianbeltran93@gmail.com>
2022-06-27 19:42:53 -0400

    Added Change Return

---
+++ Numbers CSharp/ChangeReturn.cs
@@ -0,0 +1,142 @@
+﻿namespace Numbers_CSharp
+{
+    internal static class ChangeReturn
+    {
+        internal static void ChangeReturnMethod()
+        {
+            (decimal cost, decimal moneyGiven) = ReadCostAndChangeInput();
+
+            CalculateChange(cost, moneyGiven);
+        }
+
+        private static (decimal cost, decimal moneyGiven) ReadCostAndChangeInput()
+        {
+            decimal cost = -1;
+            decimal moneyGiven = -1;
+
+            while (cost <= 0)
+            {
+                Console.WriteLine("Enter cost of item (xx.xx)\n");
+                string? userInputCost = Console.ReadLine();
+                if (decimal.TryParse(userInputCost, out cost))
+                {
+                    if (cost <= 0)
+                    {
+                        cost = -1;
+                        Console.WriteLine("Cost must be more than zero.\n");
+                    }
+                    else if (cost != decimal.Round(cost, 2))
+                    {
+                        cost = -1;
+                        Console.WriteLine("Input must have two decimal places.\n");
+                    }
+                    
+                }
+                else
+                {
+                    cost = -1;
+                    Console.WriteLine("Invalid input.\n");
+                }
+            }
+
+            while (moneyGiven < 0)
+            {
+                Console.WriteLine("Enter money given (xx.xx)\n");
+                string? userInputMoneyGiven = Console.ReadLine();
+                if (decimal.TryParse(userInputMoneyGiven, out moneyGiven))
+                {
+                    if (moneyGiven < cost)
+                    {
+                        moneyGiven = -1;
+                        Console.WriteLine("Money given must be at least as much as cost.\n");
+                    }
+                    else if (moneyGiven != decimal.Round(moneyGiven, 2))
+                    {
+                        moneyGiven = -1;
+                        Console.WriteLine("Input must have two decimal places.\n");
+                    }
+                }
+                else
+                {
+                    moneyGiven = -1;
+                    Console.WriteLine("Invalid input.\n");
+                }
+            }
+
+            return (cost, moneyGiven);
+        }
+
+        private static void CalculateChange(decimal cost, decimal moneyGiven)
+        {
+            int dollars = 0, quarters = 0, dimes = 0, nickels = 0, pennies = 0;
+            decimal countdown = moneyGiven - cost;
+            decimal change = countdown;
+
+            if (countdown == 0)
+            {
+                Console.WriteLine("You gave the exact amount. Change: $0.\n");
+                return;
+            }
+
+            CalculateDollars(ref dollars, ref countdown);
+
+            CalculateQuarters(ref quarters, ref countdown);
+
+            CalculateDimes(ref dimes, ref countdown);
+
+            CalculateNickels(ref nickels, ref countdown);
+
+            CalculatePennies(ref pennies, ref countdown);
+
+            Console.WriteLine($"Your total change is ${change}. You will receive back {dollars} dollars, {quarters} quarters, {dimes} dimes, " +
+                $"{nickels} nickels, and {pennies} pennies.\n");
+        }
+
+        private static void CalculateDollars(ref int dollars, ref decimal countdown)
+        {
+            while (countdown >= 1)
+            {
+                dollars++;
+                countdown--;
+            }
+        }
+
+        private static void CalculateQuarters(ref int quarters, ref decimal countdown)
+        {
+            while (decimal.Compare(countdown, 0.25m) != -1)
+            {
+                quarters++;
+                countdown -= 0.25m;
+            }
+        }
+
+        private static void CalculateDimes(ref int dimes, ref decimal countdown)
+        {
+            while (decimal.Compare(countdown, 0.10m) != -1)
+            {
+                dimes++;
+                countdown -= 0.10m;
+            }
+        }
+
+        private static void CalculateNickels(ref int nickels, ref decimal countdown)
+        {
+            while (decimal.Compare(countdown, 0.05m) != -1)
+            {
+                nickels++;
+                countdown -= 0.05m;
+            }
+        }
+
+        private static void CalculatePennies(ref int pennies, ref decimal countdown)
+        {
+            while (decimal.Compare(countdown, 0.01m) != -1)
+            {
+                pennies++;
+                countdown -= 0.01m;
+            }
+        }
+    }
+
+
+}
--- Numbers CSharp/Program.cs
+++ Numbers CSharp/Program.cs
@@ -1,11 +1,13 @@
-﻿while (true)
+﻿bool isProgramRunning = true;
+while (isProgramRunning)
 {
-    bool validInput = false;
+    bool isValidInput = false;
     string? userInput = null;
 
-    while (!validInput)
+    while (!isValidInput)
     {
-        Console.WriteLine("Choose one of the following with the corresponding number: \n (1) Find Pi to the nth digit \n (2) Find Fibonacci sequence to the nth digit \n");
+        Console.WriteLine("Choose one of the following with the corresponding number: \n (1) Find Pi to the nth digit \n (2) Find Fibonacci sequence to the nth digit \n " +
+            "(3) Calculate change from given cost \n");
         userInput = Console.ReadLine();
 
         switch (userInput)
@@ -13,19 +15,31 @@
             case "1":
                 Numbers_CSharp.Pi.FindPIToTheNthDigit(); //Enter a number and have the program generate PI
                                                          //up to that many decimal places. Limit = 14.
-                validInput = true;
+                isValidInput = true;
                 break;
 
             case "2":
                 Numbers_CSharp.FibonacciNumbers.FindFibonacciSequenceToTheNthDigit(); // Enter a number and have the program generate
                                                                                       // the Fibonacci sequence to that number or to the Nth number.
-                validInput = true;
+                isValidInput = true;
+                break;
+
+            case "3":
+                Numbers_CSharp.ChangeReturn.ChangeReturnMethod();       //The user enters a cost and then the amount of money given.
+                                                                        //The program will figure out the change and the number of quarters, dimes, nickels,
+                                                                        //pennies needed for the change.
+
+                isValidInput = true;
+                break;
+
+            default:
+                Console.WriteLine("Invalid input. Try again.\n");
                 break;
         }
     }
-    validInput = false;
+    isValidInput = false;
 
-    while (!validInput)
+    while (!isValidInput)
     {
         Console.WriteLine("Would you like to continue? (y/n)");
         userInput = Console.ReadLine();
@@ -34,10 +48,10 @@
             userInput = userInput.ToLower();
 
         if (userInput == "y" || userInput == "n")
-            validInput = true;
+            isValidInput = true;
     }
 
     if (userInput == "n")
-        return;
+        isProgramRunning = false;
 
 }
